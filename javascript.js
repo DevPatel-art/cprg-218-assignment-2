4844357f80310cb77d1a4187d7235940d87d0e93
DevPatel-art <dev.patel@edu.sait.ca>
2024-11-27 20:12:45 -0700

    Changes made to repositery

---
+++ javascript.js
@@ -0,0 +1,41 @@
+ps4.addEventListener('click',function(){
+    console.log('food item');    
+    document.getElementById('gaming').src='ps4.jpg';
+    document.getElementById('body').className='box';
+    document.getElementById('ps4').className='buttonstyle';
+    document.getElementById('ps5').className='buttonclick';
+    document.getElementById('old_xbox').className='buttonclick';
+    document.getElementById('new_xbox').className='buttonclick';
+    document.getElementById('description').innerHTML="The PlayStation 4 (PS4) controller also known as DualShock 4, is designed to make gaming more fun and comfortable. It has a smooth shape with easy-to-use analog sticks, buttons, and triggers, and a textured grip for better control. One of its key features is the touchpad, which lets players swipe, tap, or press for extra ways to interact with games. The light bar on the controller changes color based on the game and works with motion sensors for tracking. It also includes a small speaker for in-game sounds and a headphone jack for private listening. The Share button on the controller makes it simple to capture and share gameplay moments. The controller has built-in motion sensors, vibrates for more realistic effects, and can connect wirelessly or with a cable. Its rechargeable battery lasts for hours, and it works not just with the PS4 but also with PCs, some mobile devices, and the PS5 for PS4 games. Overall, the DualShock 4 is easy to use, packed with features, and great for all types of gamers.";
+
+})
+ps5.addEventListener('click',function(){
+    console.log('a Food item');    
+    document.getElementById('gaming').src='ps5.jpg';
+    document.getElementById('body').className='box';
+    document.getElementById('ps5').className='buttonstyle';
+    document.getElementById('ps4').className='buttonclick';
+    document.getElementById('old_xbox').className='buttonclick';
+    document.getElementById('new_xbox').className='buttonclick';
+    document.getElementById('description').innerHTML="The PlayStation 5 (PS5) controller, known as the DualSense, takes gaming to a  the next level with its advanced features and innovative design. It has a comfortable, modern look with improved analog sticks, triggers, and buttons for precise control compared to its previous version. One of its unique feature is the adaptive triggers, which can adjust resistance to make actions like pulling something or hitting the brakes feel which feels more realistic. The controller also includes haptic feedback, providing more detailed vibrations that match in-game events, like feeling raindrops or explosions.The built-in microphone lets players chat without needing a headset, and there's also a mute button for quick control. The Create button makes sharing gameplay moments easy. Like its older version, it has a touchpad and a light bar, but these are refined for better use. The 3.5mm headphone jack is still available for private gaming. The DualSense works wirelessly, charges with a USB-C cable, and has a long-lasting battery. It is designed for the PS5 but also works with PCs and mobile devices, offering a blend of comfort, innovation, and next-gen immersion for all gamers.";
+})
+old_xbox.addEventListener('click',function(){
+    console.log('old xbox controller');    
+    document.getElementById('gaming').src='old xbox.jpg';
+    document.getElementById('body').className='box';
+    document.getElementById('old_xbox').className='buttonstyle';
+    document.getElementById('ps5').className='buttonclick';
+    document.getElementById('ps4').className='buttonclick';
+    document.getElementById('new_xbox').className='buttonclick';
+    document.getElementById('description').innerHTML="The classic Xbox controller is a well-designed gaming accessory, known for its comfortable grip and sturdy build. It features two analog sticks, a D-pad, and four colored action buttons labeled A, B, X, and Y. These buttons are easy to press and responsive during gameplay. In the center, there is the iconic Xbox logo button, alongside Start and Back buttons for navigation. The triggers and bumpers on the top of the controller provide precise control, especially for racing and shooting games. Its wired connection ensures minimal lag, making it reliable for fast-paced gaming. This controller became a favorite for its ergonomic design and durable performance in the old times however its newer model has surpassed every expection of this old version.";
+})
+new_xbox.addEventListener('click',function(){
+    console.log('a Food item');    
+    document.getElementById('gaming').src='new xbox.jpg';
+    document.getElementById('body').className='box';
+    document.getElementById('new_xbox').className='buttonstyle';
+    document.getElementById('ps5').className='buttonclick';
+    document.getElementById('old_xbox').className='buttonclick';
+    document.getElementById('ps4').className='buttonclick';
+    document.getElementById('description').innerHTML="The modern Xbox controller is a sleek and refined gaming accessory, designed for comfort and precision. Its textured grips and ergonomic shape ensure a secure and comfortable hold during long gaming sessions. The controller features two smooth analog sticks, a responsive D-pad with a unique faced design, and the classic A, B, X, and Y buttons. At the center, the Xbox logo button is accompanied by a dedicated Share button for capturing and sharing gameplay moments just like ps4 and ps5. The triggers and bumpers have an enhanced tactile feel, providing greater control and feedback. With both wired and wireless options, this controller is versatile and works seamlessly with Xbox consoles, PCs, and mobile devices, offering an immersive and modern gaming experience. It has some unique competitive features which is in clash with its rival playstation ";
+})
